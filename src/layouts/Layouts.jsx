ef276ccb55fa3344d774b2d41e6674ad81a2d654
Nandakumar T <nandakumart07@gmail.com>
2024-03-14 22:10:41 +0530

    initial set

--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
+import Routers from '../routes/Routers'
 
 function Layouts() {
   return (
