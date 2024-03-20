b9d0fd2b7a654f9a8ac316dab007e7c1dfc37dfd
Nandakumar T <nandakumart07@gmail.com>
2024-03-20 23:47:20 +0530

    navigation setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import logo from '../../assets/images/logo.png'
+import userImage from '../../assets/images/avatar-icon.png'
+import { Link, NavLink } from 'react-router-dom'
+
+const navLinks=[
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/doctors',
+    display:'Find a Doctor'
+  },
+  {
+    path:'/services',
+    display:'Services'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
+  },
+
+]
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+  return <header className='header flex items-center'>
+    <div className="container">
+      <div className="flex items-center justify-between">
+        {/* --------- log ------- */}
+        <div>
+          <img src={logo} alt="" />
+        </div>
+
+        {/* ------- menu ------------ */}
+        <div className="navigation">
+          <ul className="menu flex items-center gap-[2.7rem]">
+            {
+              navLinks.map((link,index)=><li key={index}>
+                <NavLink to={link.path}
+                className={navClass=>
+                  navClass.isActive
+                    ?"text-primaryColor text-[16px] leading-7 font-[600]"
+                    :"text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
+                }
+                >{link.display}</NavLink>
+              </li>)
+            }
+          </ul>
+        </div>
+
+        {/* ----------- nav right ----------  */}
+        <div className="flex items-center gap-4">
+          <div>
+            <Link to={'/'}>
+              <figure className='w-[35px] h-[35px] rounded-full'>
+                <img src={userImage} alt="" />
+              </figure>
+            </Link>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </header>
 }
 
 export default Header
