19203c4c489db1ef8e086950a05e3daeb030bd6f
Nandakumar T <nandakumart07@gmail.com>
2024-03-26 13:07:43 +0530

    doctors section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
 import About from '../components/About/About'
 import ServiceList from '../components/Services/ServiceList'
+import DoctorList from '../components/Doctors/DoctorList'
 
 function Home() {
   return <>
@@ -241,6 +242,25 @@ function Home() {
       </div>
     </section>
     {/* -------- feature section end ------------ */}
+
+    {/* -------- our great doctors ----------- */}
+
+    <section>
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className='heading text-center'>Our great Docotrs</h2>
+          <p className="text__para text-center">
+            World calss care for everyone. Our health system offers unmatched,
+            expert health care.
+          </p>
+        </div>
+
+        <DoctorList />
+
+      </div>
+    </section>
+
+    {/* ---------- our great doctors --------- */}
 
   </>
 }
